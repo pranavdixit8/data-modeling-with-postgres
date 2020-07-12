7a284ddb6787665f38d4162175f434f77471dde8
Pranav Dixit <pranavdixit08@gmail.com>
2020-07-11 19:26:48 -0700

    add the file to create all the tables for the project

---
+++ create_tables.py
@@ -0,0 +1,93 @@
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+
+"""
+
+Fact Table
+songplays - records in log data associated with song plays i.e. records with page NextSong
+songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+Dimension Tables
+users - users in the app
+user_id, first_name, last_name, gender, level
+songs - songs in music database
+song_id, title, artist_id, year, duration
+artists - artists in music database
+artist_id, name, location, latitude, longitude
+time - timestamps of records in songplays broken down into specific units
+start_time, hour, day, week, month, year, weekday
+
+"""
+
+
+
+def create_database():
+    """
+    - Creates and connects to the sparkifydb
+    - Returns the connection and cursor to sparkifydb
+    """
+    
+    # connect to default database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+#     cur.execute("""SELECT pg_terminate_backend(pg_stat_activity.pid)
+#     FROM pg_stat_activity
+#     WHERE pg_stat_activity.datname = 'sparkifydb'
+#       AND pid <> pg_backend_pid();""")
+    
+    # create sparkify database with UTF8 encoding
+    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+
+    # close connection to default database
+    conn.close()    
+    
+    # connect to sparkify database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+    
+    return cur, conn
+
+
+def drop_tables(cur, conn):
+    """
+    Drops each table using the queries in `drop_table_queries` list.
+    """
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def create_tables(cur, conn):
+    """
+    Creates each table using the queries in `create_table_queries` list. 
+    """
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    """
+    - Drops (if exists) and Creates the sparkify database. 
+    
+    - Establishes connection with the sparkify database and gets
+    cursor to it.  
+    
+    - Drops all the tables.  
+    
+    - Creates all tables needed. 
+    
+    - Finally, closes the connection. 
+    """
+    cur, conn = create_database()
+    
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
